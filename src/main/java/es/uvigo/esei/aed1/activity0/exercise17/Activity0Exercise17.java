1bdcc9d43ae1aff4504b0937c26705c7e32fbbff
Alejandro Rosero <alejraw9@gmail.com>
2026-02-10 17:15:45 +0100

    Ejercicio 11 practica0(Exercise17)

--- src/main/java/es/uvigo/esei/aed1/activity0/exercise17/Activity0Exercise17.java
+++ src/main/java/es/uvigo/esei/aed1/activity0/exercise17/Activity0Exercise17.java
@@ -1,14 +1,16 @@
 
 package es.uvigo.esei.aed1.activity0.exercise17;
 
+import java.nio.channels.Pipe.SourceChannel;
 import java.util.Scanner;
-
 
 public class Activity0Exercise17 {
   static Scanner input = new Scanner(System.in);
-  //exercise 17
+
+  // exercise 17
   /**
    * muestra un mensaje por pantalla y lee un número entero de teclado
+   * 
    * @param message el mensaje a visualizar
    * @return el número entero leido de teclado
    */
@@ -16,9 +18,10 @@ public class Activity0Exercise17 {
     System.out.println(message);
     return Integer.parseInt(input.nextLine());
   }
-  
+
   /**
    * muestra un mensaje por pantalla y lee un número real de teclado
+   * 
    * @param message el mensaje a visualizar
    * @return el número real leido de teclado
    */
@@ -27,26 +30,40 @@ public class Activity0Exercise17 {
     return Double.parseDouble(input.nextLine());
   }
 
- 
   /**
    * muestra un mensaje por pantalla y lee una matriz de números reales
-   * @param matrix que almacena números reales
+   * 
+   * @param matrix  que almacena números reales
    * @param message el mensaje a visualizar
    */
   public static void readNotes(double[][] matrix, String message) {
     System.out.println(message);
+
     for (int i = 0; i < matrix.length; i++) {
-      matrix[i][i] = readDouble("Ingresa numero");
+      for (int j = 0; j < matrix[i].length; j++) {
+        matrix[i][j] = readDouble("enter the note " + (j + 1) + ": ");
+      }
     }
   }
-  
+
   /**
-   * muestra por pantalla un mensaje y el contenido de una matriz de números reales
-   * @param matrix que almacena numeros reales
+   * muestra por pantalla un mensaje y el contenido de una matriz de números
+   * reales
+   * 
+   * @param matrix  que almacena numeros reales
    * @param message el mensaje a visualizar
    */
-  public static void printNotes(double[][] matrix, String message){
-    
+  public static void printNotes(double[][] matrix, String message) {
+    System.out.println(message);
+    System.out.println("\n---------");
+
+    for (int i = 0; i < matrix.length; i++) {
+      System.out.println("Student " + (i + 1));
+      for (int j = 0; j < matrix[i].length; j++) {
+        System.out.print(matrix[i][j] + "|");
+      }
+      System.out.println("\n---------");
+    }
     input.close();
   }
 }
