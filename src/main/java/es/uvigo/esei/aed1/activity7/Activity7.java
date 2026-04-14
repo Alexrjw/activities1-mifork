6d010af2a9cf5d6bedca29235d8b5bdfe51b8ce3
Alejandro <alejraw9@gmail.com>
2026-04-14 17:54:22 +0200

    Actividad 7.10 creada

--- src/main/java/es/uvigo/esei/aed1/activity7/Activity7.java
+++ src/main/java/es/uvigo/esei/aed1/activity7/Activity7.java
@@ -1,5 +1,6 @@
 package es.uvigo.esei.aed1.activity7;
 
+import es.uvigo.esei.aed1.activity7.hospital.Doctor;
 import es.uvigo.esei.aed1.activity7.hospital.Hospital;
 import es.uvigo.esei.aed1.tads.list.IteratorList;
 import es.uvigo.esei.aed1.tads.list.LinkedList;
@@ -69,7 +70,7 @@ public class Activity7 {
     boolean contain = false;
 
     for (Terna<T> terna : list) {
-      if (terna.getValue().equals(value)) 
+      if (terna.getValue().equals(value))
         return contain = true;
     }
     return contain;
@@ -84,14 +85,14 @@ public class Activity7 {
 
     for (T t : list1) {
       if (!countTuplesWithAValue(summaryList, t)) {
-        summaryList.addLast(new Terna<>(t, countValueAppearances(list1, t), countValueAppearances(list2, t)));        
-      }      
+        summaryList.addLast(new Terna<>(t, countValueAppearances(list1, t), countValueAppearances(list2, t)));
+      }
     }
 
     for (T t : list2) {
       if (!countTuplesWithAValue(summaryList, t)) {
-        summaryList.addLast(new Terna<>(t, countValueAppearances(list1, t), countValueAppearances(list2, t)));        
-      }      
+        summaryList.addLast(new Terna<>(t, 0, countValueAppearances(list2, t)));
+      }
     }
 
     return summaryList;
@@ -99,7 +100,19 @@ public class Activity7 {
 
   // Exercise 7
   public static int countValuesEqualSumPreceding(List<Integer> list) throws NullPointerException {
-    return 0;
+    if (list == null) {
+      throw new NullPointerException();
+    }
+    int couter = 0, suma = 0;
+
+    for (Integer number : list) {
+      if (number == suma) {
+        couter ++;
+      }
+      suma += number;
+    }
+
+    return couter;
   }
 
   // Exercise 8
@@ -114,7 +127,19 @@ public class Activity7 {
 
   // Exercise 10
   public static Integer getMorePatients(Hospital chuo) {
-    return null;
+
+    Iterator<Doctor> doctores = chuo.getDoctors().iterator();
+    
+    Doctor doctorMayor = doctores.next();
+
+    while (doctores.hasNext()) {  
+      Doctor doctorSiguiente = doctores.next();
+      if (doctorMayor.getPatients().size() <= doctorSiguiente.getPatients().size()) {
+        doctorMayor = doctorSiguiente;        
+      }
+    }
+
+    return doctorMayor.getNumero();
   }
 
   // Exercise 11
